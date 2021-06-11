8e6f65049c1f0db2b1133ff1ae219a432ddc6c7c
Nikolina Pelivan <nikolina_pelivan@hotmail.com>
2021-06-11 10:50:15 +0200

    some text changes and style improvements

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 import Nature1 from "../assets/nature1.jpeg";
 import Nature2 from "../assets/nature2.jpeg";
 import Nature3 from "../assets/nature3.jpeg";
+import Nature4 from "../assets/nature4.jpeg";
 import Aboutme from "../assets/undraw_about_me.svg";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 import "../styles/components/_about.scss";
 
@@ -21,16 +25,16 @@ export default class About extends Component {
 					<div className="text-container">
 						<p className="block-item">
 							I'm Frontend developer & designer located in Croatia. I design,
-							develop, enhance, test and deploy web sites & apps with an end
-							goal of creating user-frendly site layout and function. I'm well
-							organised person, problem solver, freelancer with high attention
-							to detail.
+							develop, enhance, test and deploy websites & apps with the end
+							goal of creating a user-friendly site layout and function. I'm a
+							well-organised person, problem solver, freelancer with high
+							attention to detail.
 						</p>
 
 						<p className="block-item">
-							I don't like to define myself by work I've done. I define myself
-							by the work I want to do. Every day I challenge myself to do
-							project small or big because there is always something new to
+							I don't like to define myself by the work I've done. I define
+							myself by the work I want to do. Every day I challenge myself to
+							do project small or big because there is always something new to
 							learn.
 						</p>
 
@@ -42,37 +46,80 @@ export default class About extends Component {
 					</div>
 				</section>
 
-				<h2>What do you bring to the table?</h2>
-				<p>It is my skils that make me a better developer.</p>
-				<span>Skills:</span>
-				<ul>
-					<li>HTML</li>
-					<li>CSS</li>
-					<li>SCSS</li>
-					<li>Bootstrap</li>
-					<li>JavaScript</li>
-					<li>jQuery</li>
-					<li>Pugjs</li>
-					<li>Reactjs</li>
-					<li>Mobx (state management)</li>
-					<li>Git</li>
-					<li>Figma (design)</li>
-					<li>Photoshop (design)</li>
-				</ul>
+				<section className="skill-container">
+					<h2>What do you bring to the table?</h2>
+					<p>It is my skils that make me a better developer.</p>
+					<span>Skills:</span>
+					<ul>
+						<li>
+							<FontAwesomeIcon icon={faPlus} />
+							HTML
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faPlus} />
+							CSS
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faPlus} />
+							SCSS
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faPlus} />
+							Bootstrap
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faPlus} />
+							JavaScript
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faPlus} />
+							jQuery
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faPlus} />
+							Pugjs
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faPlus} />
+							Reactjs
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faPlus} />
+							Mobx (state management)
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faPlus} />
+							Git
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faPlus} />
+							Figma (design)
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faPlus} />
+							Photoshop (design)
+						</li>
+					</ul>
+				</section>
 
-				<p>
-					Since my early childhood I have enjoyed drawing and doing creative
-					things. I love to photograph nature, playing games, reading books. I
-					could also say that I program for a hobby.
-				</p>
+				<section className="hobby-container">
+					<h2>Hobbys</h2>
+					<p>
+						Since my early childhood, I have enjoyed drawing and doing creative
+						things. I love to photograph nature, playing games, reading books. I
+						could also say that I program as a hobby.
+					</p>
 
-				<img src={Nature1} alt="Sea with cloudy sky" height="100" />
-				<img
-					src={Nature2}
-					alt="Beautiful view of city with cloudy sky and sun is trying to penetrate the clouds"
-					height="100"
-				/>
-				<img src={Nature3} alt="Where the sky touches the sea" height="100" />
+					<img src={Nature1} alt="Sea with cloudy sky" height="100" />
+					<img
+						src={Nature2}
+						alt="Beautiful view of city with cloudy sky and sun is trying to penetrate the clouds"
+						height="100"
+					/>
+					<img src={Nature3} alt="Where the sky touches the sea" height="100" />
+
+					<img src={Nature4} alt="Old remains of old roman city" height="100" />
+				</section>
 			</main>
 		);
 	}
